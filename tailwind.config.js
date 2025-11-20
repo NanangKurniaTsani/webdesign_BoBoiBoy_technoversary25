f554f3a8cea49c73105a5ded53a1bb7425b4036a
NanangKurniaTsani <githubnanangkurniatsanie@gmail.com>
2025-11-21 03:07:05 +0700

    memperbaiki masalah css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.{html,js}",
-    "./src/**/*.{html,js,css}"
-  ],
+ content: [
+  "./*.html",
+  "./js/**/*.js",
+  "./src/**/*.{html,js,css}"
+]
+,
   theme: {
     extend: {
       colors: {
